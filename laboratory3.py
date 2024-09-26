305ebb80910fe8e5c8d692ad8ca2e579bc6be589
KenLustre <kenlouismj@gmail.com>
2024-09-26 09:43:37 +0800

    Update laboratory3.py

--- laboratory3.py
+++ laboratory3.py
@@ -1,3 +1,15 @@
-sal = input("Enter your salary = ")
-loan = input("Enter loan requested amount = ")
-
+sal = float(input("Enter your Monthly Salary = "))
+loan = float(input("Enter Loan Request Amount = "))
+sec = sal * 10
+if sal >= 30000 and loan <= sec:
+    print("You are eligible to have a loan!")
+    loan_interest = loan * 1.10
+    month = int(input("How many months will you pay your loan? = "))
+    monthly_payment = loan / month
+    print("Your monthly pay with 10% Interest will be", monthly_payment)
+else:
+     print("You are not eligible for a loan.")
+     if sal < 30000:
+        print("Reason: Insufficient salary amount.")
+     if loan > sec:
+        print("Reason: The requested loan amount is too high.")
